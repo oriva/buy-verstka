942b7f17cb124a908671928ebfd16e8c9aa4a38f
Oriva <weelman93@gmail.com>
2018-12-02 20:53:15 +0800

    save

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-var swiper = new Swiper('.swiper-container');
+var swiper = new Swiper('.big-swiper', {
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+});
+
+var swiper = new Swiper('.swiper-container-promo', {
+    pagination: '.swiper-container-promo',
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    coverflow: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
